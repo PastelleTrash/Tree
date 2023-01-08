07b988c4c19c8343df9a34ba43bd9e121c3c2128
Anton ulrik <contactpastelletrash@gmail.com>
2023-01-08 15:24:55 +0100

    added n edited everything

--- assets/config.js
+++ assets/config.js
@@ -1,47 +1,27 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Pastelle - Links",
+  "Name": "Pastelle",
+  "Description": "🌈 rockstar ⭐ trans girl 💫 who is not afraid to get a little dirty 👅🔥",
   "links": [
     {
-      "Title": "Website",
-      "URL": "http://www.harindu.dev",
+      "Title": "OnlyFans",
+      "URL": "https://onlyfans.com/pastelle_trash",
       "icon_classes": "fas fa-link",
     },
     {
-      "Title": "Github",
-      "URL": "https://github.com/harindulk",
-      "icon_classes": "fab fa-github",
-    },
-    {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
       "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
+      "URL": "https://twitter.com/Pastelle_Trash",
+      "icon_classes": "fab fa-twitter",
     },
     {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
+      "Title": "Reddit",
+      "URL": "https://www.reddit.com/user/Pastelle_Trash",
+      "icon_classes": "fab fa-reddit"
     },
     {
-      "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
+      "Title": "Instagram",
+      "URL": "http://Instagram.com/pastelle_trash",
+      "icon_classes": "fab fa-instagram"
     },
   ]
 }
